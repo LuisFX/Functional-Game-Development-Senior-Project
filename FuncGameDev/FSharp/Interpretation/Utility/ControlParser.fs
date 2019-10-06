575c2ff48a2522ab48df5858db778a32fa38d18b
August B Dailey <daileyab@rose-hulman.edu>
2019-10-06 19:51:57 -0400

    Created ControlParser for parsing user input config file

---
+++ FuncGameDev/FSharp/Interpretation/Utility/ControlParser.fs
@@ -0,0 +1,16 @@
+﻿module ControlParser
+
+open System.IO
+open System
+
+type ControlParser() =
+    member x.Read() =
+        use stream = new StreamReader @"C:\Users\daileyab\Desktop\"
+
+        let mutable valid = true
+        while (valid) do
+            let line = stream.ReadLine()
+            if (line = null) then
+                valid <- false
+            else
+                printfn "%A" line
